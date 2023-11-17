f0b138ff6050f1647211ac9e511636d1027dd7de
googjohn <120353235+googjohn@users.noreply.github.com>
2023-11-17 23:58:54 +0800

    tikitakatoko

---
+++ src/components/Board.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Square } from "./Square";
+
+export const Board = ({ squares, onClick }) => {
+  return (
+    <div className="board">
+      {squares.map((square, i) => (
+        <Square key={i} value={square} onClick={() => onClick(i)} />
+      ))}
+    </div>
+  );
+};
+
